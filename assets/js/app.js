75ab2129e2a54e1ae73d47ed236e12b8cea463ab
Mehdimblr@gmail.com <Mehdimblr@gmail.com>
2022-04-08 02:37:59 +0430

    added name checker and bio detail

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,68 @@
 const API_URL = "https://api.github.com/users/";
 const twitterProfile = document.querySelector("#twitter-profile")
-const name = document.querySelector("#twitter-name");
+const nameGithub = document.querySelector("#twitter-name");
 const userName = document.querySelector("#twitter-username");
 const profileLink = document.querySelector("#profile-link")
 const twitterPost = document.querySelector("twitter-post");
 const followers = document.querySelector("#twitter-follower")
 const following = document.querySelector("#twitter-following")
 const hireable = document.querySelector("#badge")
-
+const gitBio = document.querySelector("#gitBio");
 let submit = document.querySelector("#submit");
 let inputText = document.querySelector("#inputName");
 // const inputValue = inputText.value;
 const clickFunc = () => {
-        if (!inputText.value) {
-            toasti("you didn't enter any chars");
-            return;
-        } else {
-            fetch(API_URL + inputText.value, { method: "GET" })
-                .then(function(response) {
-                    return response.json();
-                })
-                .then((data) => {
-                    if (data.message == "Not Found") {
-                        toasti("This username has not been registered yet");
-                        return;
+    if (!inputText.value) {
+        toasti("you didn't enter any chars");
+        return;
+    } else {
+        fetch(API_URL + inputText.value, { method: "GET" })
+            .then(function(response) {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.message == "Not Found") {
+                    toasti("This username has not been registered yet");
+                    return;
 
-                    } else {
-                        console.log(data)
-                        name.innerHTML = data.name;
-                        userName.innerHTML = "@" + data.login;
-                        followers.innerHTML = data.followers;
-                        following.innerHTML = data.following;
-                        twitterProfile.src = data.avatar_url;
-                        hireableCheck(data.hireable);
-                    }
+                } else {
+                    console.log(data);
+                    nameCheck(data.name);
+                    gitBioCkeck(data.bio);
+                    userName.innerHTML = "@" + data.login;
+                    followers.innerHTML = data.followers;
+                    following.innerHTML = data.following;
+                    twitterProfile.src = data.avatar_url;
+                    hireableCheck(data.hireable);
+                }
 
-                });
+            });
 
 
-        }
     }
-    // hireable func
+}
+
+//Git Hib bio Ckeck
+function gitBioCkeck(gitBioStatus) {
+    if (!gitBioStatus) {
+        gitBio.innerHTML = "";
+        return;
+    } else {
+        gitBio.innerHTML = gitBioStatus;
+    }
+}
+// name check
+function nameCheck(dataName) {
+    if (!dataName) {
+        nameGithub.innerHTML = "Not Found this user's name";
+
+    } else {
+        nameGithub.innerHTML = dataName;
+    }
+}
+// hireable func
 function hireableCheck(hireStatus) {
-    if (!hireStatus) {
+    if (hireStatus == false) {
         hireable.classList.add("fa-window-close")
     } else {
         hireable.classList.add("fa-check-square")
@@ -63,3 +83,6 @@ const toasti = (textValue) => {
         }
     }).showToast();
 }
+
+
+//tootlip with popper
